da1b9eeabdde93ac7b6a027e4f377d7e99f0c731
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-09-20 09:43:31 +0700

    display last message on sidebar chat

--- src/components/Sidebar Chat/SidebarChat.jsx
+++ src/components/Sidebar Chat/SidebarChat.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Avatar } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
@@ -6,6 +6,20 @@ import "./sidebar-chat.css";
 import db from "../../firebase";
 
 const SidebarChat = ({ addNewChat, name, id }) => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    if (id) {
+      db.collection("rooms")
+        .doc(id)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }
+  }, [id]);
+
   const createChat = () => {
     const roomName = prompt("Please enter name");
     if (roomName) {
@@ -21,7 +35,7 @@ const SidebarChat = ({ addNewChat, name, id }) => {
         <Avatar></Avatar>
         <div className="sidebarChat__info">
           <h2>{name}</h2>
-          <p>Last message...</p>
+          <p>{messages[0]?.message}</p>
         </div>
       </div>
     </Link>
